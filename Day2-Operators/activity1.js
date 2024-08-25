44d133e3f399e4b5ee1b45a8d3f70b6e3198ac00
lakshminarayana1205 <lakshminarayanajeripotula@gmail.com>
2024-08-25 11:33:07 +0530

    Day2 Done

---
+++ Day2-Operators/activity1.js
@@ -0,0 +1,33 @@
+// Day 2: Operators
+// Tasks/Activities:
+// Activity 1: Arithmetic Operations
+// + Task 1: Write a program to add two numbers and log the result to the console.
+
+let num1=7;
+let num2=5;
+console.log(num1+num2);
+
+
+
+// Task 2: a program to subtract two numbers and log the result to the conole.
+
+console.log(num1-num2);
+
+
+
+
+
+// + Task 3: Write a program to multiply two numbers and log the result to the console.
+console.log(num1*num2);
+
+
+
+// + Task 4: Write a program to divide two numbers and log the result to the console.
+
+console.log(num1/num2);
+
+
+
+// Task 5: Write a program to find the remainder when one number is divided by another and log the rest to the conole.
+
+console.log(num1%num2);
---
+++ Day2-Operators/activity2.js
@@ -0,0 +1,14 @@
+// Activity 2: Assignment Operators
+// « Task 6: Use the += operator to add a number to a variable and log the result to the console.
+
+let num1=3;
+num1+=8;
+console.log(num1);
+
+
+
+
+// + Task 7: Use the -= operator to subtract a number from a variable and log the result to the console.
+let num2=5;
+num2-=2;
+console.log(num2);
